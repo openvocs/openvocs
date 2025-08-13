e07f092b8927a4a842ed8dfc48c9b186b3c0f9dc
Michael J. Beer <michael.beer@dlr.de>
2025-08-13 16:50:19 +0200

    SIP replay replays SIP serde logs.

--- src/tools/ov_sip_replay/src/ov_sip_replay.c
+++ src/tools/ov_sip_replay/src/ov_sip_replay.c
@@ -23,66 +23,280 @@
 
         ------------------------------------------------------------------------
 */
-#include <stdio.h>
+#include <ov_base/ov_buffer.h>
+#include <ov_base/ov_socket.h>
 #include <ov_base/ov_utils.h>
 #include <stdarg.h>
-#include <ov_base/ov_socket.h>
-#include <ov_base/ov_buffer.h>
+#include <stdio.h>
 
 /*----------------------------------------------------------------------------*/
 
 FILE *outstream = 0;
+FILE *err_outstream = 0;
 
 /*----------------------------------------------------------------------------*/
 
-static void print(char const *msg, ...) {
+typedef enum { SND, RCV, INVALID } State;
+
+State state_to_replay = RCV;
+
+/*----------------------------------------------------------------------------*/
+
+static int print_timestamp_nocheck(FILE *msg_out) {
+
+    struct timespec ts = {0};
+
+    if (!timespec_get(&ts, TIME_UTC)) {
+        return -1;
+    }
+
+    struct tm broken_down_time = {0};
+
+    if (0 == gmtime_r(&ts.tv_sec, &broken_down_time)) {
+        return -1;
+    }
+
+    char time_str[25] = {0};
+
+    if (!strftime(time_str, sizeof(time_str), "%FT%T", &broken_down_time)) {
+        return -1;
+    }
+
+    return fprintf(msg_out, "%s.%.6ldZ", time_str, ts.tv_nsec);
+}
+
+/*----------------------------------------------------------------------------*/
+
+static void print_to(FILE *out, char const *msg, ...) {
 
     va_list args;
     va_start(args, msg);
 
-    fprintf(outstream, "%lld: ", (long long)time(0));
-    vfprintf(outstream, msg, args);
+    print_timestamp_nocheck(out);
+    vfprintf(out, msg, args);
 
     va_end(args);
 
 }
 
+#define print(...) print_to(outstream, __VA_ARGS__)
+#define printerr(...) print_to(err_outstream, __VA_ARGS__)
+
+//static void print(char const *msg, ...) {
+//    va_list args;
+//    va_start(args, msg);
+//    print_to(outstream, msg, args);
+//    va_end(args);
+//}
+//
+//static void printerr(char const *msg, ...) {
+//    va_list args;
+//    va_start(args, msg);
+//    print_to(err_outstream, msg, args);
+//    va_end(args);
+//}
+
 /*----------------------------------------------------------------------------*/
 
-static ov_buffer *get_next_message(FILE *input) {
+static char const *state_to_str(State state) {
+    switch (state) {
+        case SND:
+            return "SND";
+        case RCV:
+            return "RCV";
+        default:
+            return "INVALID";
+    }
+}
 
-    UNUSED(input);
+/*----------------------------------------------------------------------------*/
 
-    size_t period = 10; // MUST be bigger than 8...
-    size_t len = 100 * 1000 * 100 * period;
-    char *str = calloc(1, len);
+static unsigned char get_char(FILE *input) {
+    int i = fgetc(input);
 
-    for(size_t i = 0; i < len; i += period) {
+    if ((i < 0) || (i == EOF)) {
+        printerr("During reading: Error or EOF occured\n");
+        exit(EXIT_SUCCESS);
+    } else {
+        return (unsigned char)i;
+    }
+}
 
-        str[i] = 'a';
-        str[i + 1] = 'b';
-        str[i + 2] = 'a';
-        str[i + 5] = '1';
-        str[i + 6] = '9';
-        str[i + 7] = '1';
+/*----------------------------------------------------------------------------*/
 
+static unsigned char line[10000] = {0};
+
+// Always points to end of string insofar as line[line_len] = 0 is the
+// terminal zero == first char following message
+static size_t line_len = 0;
+
+static size_t max_line_len = sizeof(line) / sizeof(line[0]);
+
+#define CR '\r'
+#define LF '\n'
+
+static size_t read_line(FILE *input) {
+    // in here, always points to the NEXT slot to write to
+    size_t i = 0;
+
+    char c = get_char(input);
+    while (c != 0) {
+        line[i++] = c;
+        if (c == LF) {
+            break;
+        } else {
+            c = get_char(input);
+        }
+
+        if (i >= max_line_len) {
+            printerr(
+                "Line way too long %zu chars - surpasing maximum chars of %zu",
+                i, max_line_len);
+            abort();
+        }
+    };
+
+    line[i] = 0;
+    line_len = i;
+
+    printerr("Got line '%s'\n", line);
+
+    return i;
+}
+
+/*----------------------------------------------------------------------------*/
+
+static State line_to_state() {
+    bool digits_found = false;
+
+    size_t i = 0;
+    while (i < line_len) {
+        if (0 != strchr("0123456789", line[++i])) {
+            digits_found = true;
+        } else {
+            break;
+        }
     }
 
-    ov_buffer *buf = ov_buffer_create(len);
-    buf->start = (uint8_t *)str;
-    buf->length = len;
+    if (!digits_found) {
+        printerr("No digits found in %s\n", line);
+        return INVALID;
+    }
+
+    printerr("Digits in %s\n", line);
+    // Now should come a string like <XXX>\n
+
+    // So we require at least 6 more chars, and next char should be a '<'
+    if (((i + 5) > line_len) || (line[i] != '<')) {
+        printerr(
+            "After digits: Line too short or '<' missing: %s  - %c should be "
+            "'<'\n",
+            line, line[i]);
+        return INVALID;
+    }
+
+    printerr("After digits: Line long enough and '<' found: %s\n", line);
+
+    if (line[i + 1] == 'S') {
+        // So the line is either invalid or SND
+        if ((line[i + 2] == 'N') && (line[i + 3] == 'D') &&
+            (line[i + 4] == '>')) {
+            return SND;
+        } else {
+            return INVALID;
+        }
+
+    } else if (line[i + 1] == 'R') {
+        // So the line is either invalid or RCV
+        if ((line[i + 2] == 'C') && (line[i + 3] == 'V') &&
+            (line[i + 4] == '>')) {
+            return RCV;
+        } else {
+            return INVALID;
+        }
+
+    } else {
+        // Other garbage
+        return INVALID;
+    }
+}
+
+/*----------------------------------------------------------------------------*/
+
+static State skip_header(FILE *input) {
+    State state = INVALID;
+
+    while (true) {
+        size_t len = read_line(input);
+
+        // Check if we can skip the line - We can skip anything that does not
+        // end in CR LF
+
+        if ((len > 1) && (CR == line[len - 2]) && (LF == line[len - 1])) {
+            printerr("Detected start of %s message\n", state_to_str(state));
+            return state;
+        } else {
+            state = line_to_state();
+        }
+
+        printerr("Skipped %s - state is %s\n", line, state_to_str(state));
+        line[0] = 0;
+        line_len = 0;
+    }
+
+    return state;
+}
+
+/*----------------------------------------------------------------------------*/
+
+static ov_buffer *get_next_message(FILE *input, char *first_line) {
+    State state = INVALID;
+
+    while (state != state_to_replay) {
+        state = skip_header(input);
+
+        printerr("get_next_message: Got a %s message\n", state_to_str(state));
+    }
+
+    // Now the next line is already in the line - buffer
+    printerr("get_next_message: Creating message to send...\n\n\n");
+
+    size_t total_len = 0;
+    char message[1000 * 1000] = {
+        0};  // That's one MB, should suffice for a SIP message
+
+    char *write_ptr = message;
+
+    if (0 != first_line) {
+        memcpy(first_line, line, line_len);
+    }
+
+    while ((line_len > 1) && (CR == line[line_len - 2]) &&
+           (LF == line[line_len - 1])) {
+        memcpy(write_ptr, line, line_len);
+        write_ptr += line_len;
+        total_len += line_len;
+
+        line[0] = 0;
+        line_len = 0;
+
+        read_line(input);
+    }
+
+    ov_buffer *buf = ov_buffer_create(total_len);
+    memcpy(buf->start, message, total_len);
+    buf->length = total_len;
 
     return buf;
-
 }
 
 /*----------------------------------------------------------------------------*/
 
 int main(int argc, char *argv[]) {
+    outstream = stdout;
+    err_outstream = stderr;
 
-    outstream = stderr;
-
-    char const *input_file  = "STDIN";
+    char const *input_file = "STDIN";
     FILE *input = stdin;
 
     uint32_t delay_secs = 5;
@@ -93,13 +307,13 @@ int main(int argc, char *argv[]) {
         .type = TCP,
     };
 
-    if((argc > 1) && (0 != strcmp(argv[1], "-"))) {
+    if ((argc > 1) && (0 != strcmp(argv[1], "-"))) {
         input_file = argv[1];
         input = fopen(input_file, "r");
     }
 
-    if(0 == input) {
-        print("Could not open %s\n", input_file);
+    if (0 == input) {
+        printerr("Could not open %s\n", input_file);
         exit(EXIT_FAILURE);
     }
 
@@ -107,7 +321,7 @@ int main(int argc, char *argv[]) {
     int send_fd = ov_socket_create(server, true, &serr);
 
     if (0 > send_fd) {
-        print("Could not connect to %s:%" PRIu16 "  - %s\n", server.host,
+        printerr("Could not connect to %s:%" PRIu16 "  - %s\n", server.host,
               server.port, strerror(serr.err));
         exit(EXIT_FAILURE);
     }
@@ -120,10 +334,16 @@ int main(int argc, char *argv[]) {
     print("Reading from %s\n", input_file);
     print("Sending to %s:%" PRIu16 "\n", server.host, server.port);
 
-    ov_buffer *message = get_next_message(input);
+    char first_line[10000] = {0};
+    size_t first_line_capacity = sizeof(first_line) / sizeof(first_line[0]);
+
+    ov_buffer *message = get_next_message(input, first_line);
 
     while (0 != message) {
         usleep(delay_secs * 1000 * 1000);
+
+        print("Sending %s\n", first_line);
+        memset(first_line, 0, first_line_capacity);
 
         ssize_t written = 0;
         size_t to_write_len = message->length;
@@ -135,16 +355,16 @@ int main(int argc, char *argv[]) {
             if (written > -1) {
                 to_write = to_write + (size_t)written;
                 to_write_len -= (size_t)written;
-                print("Wrote %zd bytes - %zu to go...\n", written,
+                printerr("Wrote %zd bytes - %zu to go...\n", written,
                       to_write_len);
             } else {
-                print("Could not write: %s\n", strerror(errno));
+                printerr("Could not write: %s\n", strerror(errno));
                 break;
             }
         }
 
         message = ov_buffer_free(message);
-        message = get_next_message(input);
+        message = get_next_message(input, first_line);
     }
 
     fclose(input);
