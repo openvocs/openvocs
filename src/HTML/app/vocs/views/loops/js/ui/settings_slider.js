c045d858a9226f9c6714a442634ce51bf16578ab
Anja Bertard <anja.bertard@dlr.de>
2025-11-06 10:25:13 +0100

    readd HID option in vocs frontend

--- src/HTML/app/vocs/views/loops/js/ui/settings_slider.js
+++ src/HTML/app/vocs/views/loops/js/ui/settings_slider.js
@@ -31,6 +31,7 @@ import * as ov_Websockets from "/lib/ov_websocket_list.js";
 import * as ov_Vocs from "/lib/ov_vocs.js";
 
 import ov_Audio from "/lib/ov_media/ov_audio.js";
+import * as ov_WebRTCs from "/lib/ov_media/ov_webrtc_list.js";
 
 import * as PTT_Bar from "./ptt_bar.js";
 import * as Loop_View from "./loop_view.js";
@@ -41,7 +42,7 @@ const DOM = {};
 
 var mic_vis;
 
-export function init() {
+export async function init() {
     DOM.menu_button = document.getElementById("open_menu_button");
     DOM.slider_button = document.getElementById("open_settings_button");
 
@@ -51,6 +52,8 @@ export function init() {
     DOM.toggle_audio_test = DOM.slider.querySelector("#speaker_test");
     DOM.test_audio = DOM.slider.querySelector("#speaker_test_audio");
     DOM.mic_oscillation = DOM.slider.querySelector("#microphone_test").querySelector("canvas");
+    DOM.mic_list = DOM.slider.querySelector("#microphone_list");
+    DOM.speaker_list = DOM.slider.querySelector("#speaker_list");
 
     DOM.multi_talk_checkbox = document.getElementById("multi_talk_checkbox");
     DOM.mute_hardware_checkbox = document.getElementById("hardware_checkbox");
@@ -60,6 +63,7 @@ export function init() {
     DOM.mute_key_checkbox = document.getElementById("key_checkbox");
     DOM.mute_mousewheel_checkbox = document.getElementById("mousewheel_checkbox");
     DOM.add_HID = document.getElementById("add_HID");
+    DOM.hid_list = document.getElementById("hid_devices");
 
     hide_slider();
 
@@ -67,6 +71,7 @@ export function init() {
         if (!DOM.slider.open) {
             if (DOM.audio_details.open)
                 start_mic_oscillation();
+            //update_device_lists();
             DOM.slider.show();
         } else
             hide_slider();
@@ -91,6 +96,13 @@ export function init() {
         else
             DOM.test_audio.pause();
     });
+
+    // navigator.mediaDevices.ondevicechange = async (event) => {
+    //     console.log(event);
+    //     update_device_lists();
+    // };
+
+
 
     //-------------------------------------------------------------------------
     // ui options
@@ -139,12 +151,15 @@ export function init() {
     if (!navigator.hid)
         DOM.add_HID.disabled = true;
     DOM.add_HID.addEventListener("click", function () {
-        navigator.hid.requestDevice({ filters: [] }).then((devices) => {
+        navigator.hid.requestDevice({ filters: HID_FILTERS }).then((devices) => {
             window.dispatchEvent(new CustomEvent("new_hid", {
                 detail: devices[0]
             }));
+            update_hid_device_list();
         });
     });
+    update_hid_device_list();
+
 
     if (SECURE_VOICE_PTT)
         DOM.mute_browser_options.classList.add("removed");
@@ -154,6 +169,16 @@ export function init() {
     const urlParams = new URLSearchParams(window.location.search);
     const keyset = urlParams.get('keysetname')
     document.getElementById("keyset_name").innerText = keyset;
+}
+
+function update_hid_device_list() {
+    if (navigator.hid)
+        navigator.hid.getDevices().then((devices) => {
+            const device_names = devices.map(device => {
+                return device.productName || "Unknown Device";
+            }).join(", ");
+            DOM.hid_list.innerText = device_names ? device_names : "-";
+        });
 }
 
 function hide_slider() {
@@ -166,6 +191,53 @@ function hide_slider() {
 
 function open_slider() {
 
+}
+
+async function update_device_lists() {
+    await ov_WebRTCs.find_audio_devices();
+    DOM.mic_list.replaceChildren();
+    for (let mic of ov_WebRTCs.microphones) {
+        let element = document.createElement("input");
+        element.type = "radio";
+        element.id = "mic_" + mic.deviceId;
+        element.value = mic.deviceId;
+        element.name = "mic";
+        let label = document.createElement("label");
+        label.htmlFor = "mic_" + mic.deviceId;
+        label.textContent = mic.label || `Microphone ${mic.deviceId}`;
+        label.classList.add("button");
+
+        DOM.mic_list.appendChild(element);
+        DOM.mic_list.appendChild(label);
+
+        if (ov_WebRTCs.selected_microphones.includes(mic.deviceId))
+            element.checked = true;
+
+        element.addEventListener("change", () => {
+            console.log("mic", element.value);
+            ov_WebRTCs.switch_local_media_stream(element.value);
+            ov_WebRTCs.mute_outbound_stream(true);
+        });
+    }
+
+    DOM.speaker_list.replaceChildren();
+    for (let speaker of ov_WebRTCs.speakers) {
+        let element = document.createElement("input");
+        element.type = "radio";
+        element.id = "speaker_" + speaker.deviceId;
+        element.value = speaker.deviceId;
+        element.name = "speaker";
+        let label = document.createElement("label");
+        label.htmlFor = "speaker_" + speaker.deviceId;
+        label.textContent = speaker.label || `Speaker ${speaker.deviceId}`;
+        label.classList.add("button");
+
+        if (ov_WebRTCs.selected_speaker === speaker.deviceId)
+            element.checked = true;
+
+        DOM.speaker_list.appendChild(element);
+        DOM.speaker_list.appendChild(label);
+    }
 }
 
 function start_mic_oscillation() {
