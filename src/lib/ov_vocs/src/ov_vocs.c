8c17002a4572b50965cef7cb5a5c8ca54344ea50
Töpfer, Markus <markus.toepfer@dlr.de>
2025-11-20 11:10:40 +0100

    get_recordings for any user without role implemented

--- src/lib/ov_vocs/src/ov_vocs.c
+++ src/lib/ov_vocs/src/ov_vocs.c
@@ -2327,7 +2327,7 @@ static bool client_event_get_recording(ov_vocs *vocs,
     const char *user = ov_json_string_get(ov_json_get(data, "/" OV_KEY_USER));
     const char *role = ov_json_string_get(ov_json_get(data, "/" OV_KEY_ROLE));
 
-    if (!user || !role) {
+    if (!user) {
 
         send_error_response(
             vocs, input, socket, OV_ERROR_CODE_AUTH, OV_ERROR_DESC_AUTH);
