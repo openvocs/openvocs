7a365896f37d925f771a1322fb91a820fd8b6819
Töpfer, Markus <markus.toepfer@dlr.de>
2025-12-01 13:28:45 +0100

    readded ov_version_additional_info

--- src/lib/ov_base/include/ov_version.h
+++ src/lib/ov_base/include/ov_version.h
@@ -97,6 +97,7 @@
             "      build id : %s\n"                                            \
             "     commit id : %s\n"                                            \
             " Compiler      : %s\n"                                            \
+            "%s\n"                                                             \
             "------------------------------------------------------------"     \
             "----------------\n"                                               \
             "\n",                                                              \
@@ -105,7 +106,8 @@
             OV_VERSION,                                                        \
             OV_VERSION_BUILD_ID,                                               \
             OV_VERSION_COMMIT_ID,                                              \
-            OV_VERSION_COMPILER);
+            OV_VERSION_COMPILER,                                               \
+            ov_version_additional_info());
 
 /*----------------------------------------------------------------------------*/
 
@@ -121,9 +123,8 @@ inline const char *ov_version_build_date() { return OV_VERSION_BUILD_DATE; }
 
 /*----------------------------------------------------------------------------*/
 
-/*
+
 char const *ov_version_additional_info();
 bool ov_version_set_additional_info(char const *additional_info);
-*/
 
 #endif /* ov_version_h */
