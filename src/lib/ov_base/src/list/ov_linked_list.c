7e230a1052987530b1477c777c6c62d3161f91e9
Töpfer, Markus <markus.toepfer@dlr.de>
2026-03-24 15:43:29 +0100

    removed check for list cleanlyness

--- src/lib/ov_base/src/list/ov_linked_list.c
+++ src/lib/ov_base/src/list/ov_linked_list.c
@@ -641,9 +641,7 @@ static void *impl_linked_list_pop(ov_list *self) {
     if (0 == ll)
         goto no_list_error;
 
-    ov_list_dump(stderr, self);
-
-    ASSERT_LIST_INVARIANTS(ll);
+    //ASSERT_LIST_INVARIANTS(ll);
 
     ListEntity *last = ll->head.last;
 
