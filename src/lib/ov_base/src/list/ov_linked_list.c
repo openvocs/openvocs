dcdaa36c26b737302ae03273a6df1589e332846a
Töpfer, Markus <markus.toepfer@dlr.de>
2026-03-24 15:16:38 +0100

    removed ov_linked_list changes

--- src/lib/ov_base/src/list/ov_linked_list.c
+++ src/lib/ov_base/src/list/ov_linked_list.c
@@ -530,15 +530,12 @@ static bool impl_linked_list_insert(ov_list *self, size_t pos, void *item) {
     /* If the new element is the first element */
     if (entity == &ll->head) {
         new->last = NULL;
-        entity->next = new;
-        entity->last = new;
     }
 
     /* If new element is the only element in the list,
      * head.next ought to point to it ...*/
     if (NULL == ll->head.last) {
         ll->head.last = new;
-        ll->head.next = new;
     }
 
     ASSERT_LIST_INVARIANTS(ll);
