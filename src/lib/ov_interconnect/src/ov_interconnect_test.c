8fef906faa7003567431c077b8a9ee815a5bab4d
Töpfer, Markus <markus.toepfer@dlr.de>
2026-05-21 07:53:31 +0200

    commented ov_interconnect setup

--- src/lib/ov_interconnect/src/ov_interconnect_test.c
+++ src/lib/ov_interconnect/src/ov_interconnect_test.c
@@ -162,6 +162,11 @@ static bool setup_interconnect(ov_event_loop **loop_out, ov_io **io_out,
                                ov_interconnect **interconnect_out) {
 
     ov_log_debug("Setup interconnect");
+
+    UNUSED(loop_out);
+    UNUSED(io_out);
+    UNUSED(interconnect_out);
+    
 /*
     ov_event_loop *loop = ov_event_loop_default(
          (ov_event_loop_config){.max.sockets = 100, .max.timers = 100});
