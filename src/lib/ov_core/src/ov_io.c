932801e97bb287f79982ddc4210782c2303d2012
Töpfer, Markus <markus.toepfer@dlr.de>
2026-05-31 07:46:12 +0200

    added error path on SSL_ERRROR_SYSCALL

--- src/lib/ov_core/src/ov_io.c
+++ src/lib/ov_core/src/ov_io.c
@@ -1166,6 +1166,9 @@ static bool io_send_ssl(ov_io *self, Connection *conn, const ov_buffer *buffer){
     
         case SSL_ERROR_SYSCALL:
 
+            if (SSL_ERROR_ZERO_RETURN == n)
+                goto error;
+
             errorcode = ERR_get_error();
             ERR_error_string_n(errorcode, errorstring,
                                OV_SSL_ERROR_STRING_BUFFER_SIZE);
