950a80e0084ffb8d3c55945aff93f4f31034b53c
Anja Bertard <anja.bertard@dlr.de>
2026-07-28 12:33:21 +0200

    admin ui: handel error with check LDAP: if LDAP is disabled but roles are configured to be imported

--- src/HTML/app/vocs_admin/views/config_rbac/js/view.js
+++ src/HTML/app/vocs_admin/views/config_rbac/js/view.js
@@ -70,11 +70,11 @@ export function init(view_id, ldap_auth) {
 
     DOM.graph.allow_highlighted_loops = ALLOW_HIGHLIGHTED_LOOPS;
     DOM.graph.managed_users = ldap_auth.users;
-    DOM.graph.managed_roles = ldap_auth.roles;
+    DOM.graph.managed_roles = (ldap_auth.roles && ldap_auth.users);
 }
 
 export function render(domain_data, project_id, view_domain_only) {
-    if(project_id !== current_project_id){
+    if (project_id !== current_project_id) {
         current_project_id = project_id;
         let project_data = domain_data.projects[project_id];
         DOM.graph.clear();
@@ -89,11 +89,11 @@ export function render(domain_data, project_id, view_domain_only) {
             domain_id = domain_data.id;
         }
         DOM.graph.fill_out_links();
-    
+
         DOM.graph.filter_unused_users(DOM.filter_users.checked);
         DOM.graph.filter_unused_subset_roles(DOM.filter_roles.checked, domain_id);
         DOM.graph.filter_unused_subset_loops(DOM.filter_loops.checked, domain_id);
-    }       
+    }
     DOM.graph.render_edges();
     console.log("(project rbac) View rendered");
 }
